59059789bfb4b6c64e79b79781ed9235cc4cffb2
0ALEX-2 <sakilahmed965@gmail.com>
2023-04-01 10:25:22 +0530

    updated

--- todo-app/src/components/Todo.jsx
+++ todo-app/src/components/Todo.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import { BsTrash } from "react-icons/bs";
 
 const Todo = ({todo}) => {
   return (
-    <li>
-        <span>{todo.data}</span>
+    <li className='flex justify-between border w-[400px] bg-slate-300 mt-2 p-3 m-auto text-center rounded-md items-center'>
+        <div className=''>{todo.data}</div>
+        <div className='cursor-pointer flex '>
+        <BsTrash/>
+        </div>
     </li>
   )
 }
