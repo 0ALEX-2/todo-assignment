67f5dfc2c3a723685341debebe00b87a65260a84
0ALEX-2 <sakilahmed965@gmail.com>
2023-04-02 09:18:50 +0530

    env file

--- backend/Config/db.js
+++ backend/Config/db.js
@@ -3,10 +3,11 @@ import mongoose from "mongoose"
 const connectDB=async ()=>{
 
     //for security purpose it is not safe to use confidential url, we should use .env 
-    // const URI='mongodb://sakilahmed965:shakiltodoapp123@ac-mtx2wf6-shard-00-00.qapjxpm.mongodb.net:27017,ac-mtx2wf6-shard-00-01.qapjxpm.mongodb.net:27017,ac-mtx2wf6-shard-00-02.qapjxpm.mongodb.net:27017/todoApp?replicaSet=atlas-116cc3-shard-0&ssl=true&authSource=admin'
+     const URI='mongodb://sakilahmed965:shakiltodoapp123@ac-mtx2wf6-shard-00-00.qapjxpm.mongodb.net:27017,ac-mtx2wf6-shard-00-01.qapjxpm.mongodb.net:27017,ac-mtx2wf6-shard-00-02.qapjxpm.mongodb.net:27017/todoApp?replicaSet=atlas-116cc3-shard-0&ssl=true&authSource=admin'
     
     try {
-        const connect=await mongoose.connect(process.env.MONGO_URI)
+        // const connect=await mongoose.connect(process.env.MONGO_URI)
+        const connect=await mongoose.connect(URI)
         console.log(`Connected to MongoDB database ${connect.connection.host}`)
     } catch (error) {
         console.log(`Error in MongoDB ${error}`);
